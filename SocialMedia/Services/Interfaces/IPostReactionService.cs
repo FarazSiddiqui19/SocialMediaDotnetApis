aa2e43bbc800a32bdb197ec1242b57b20cef2b94
FarazUniGoa <mca2024.faraz@unigoa.ac.in>
2025-12-26 17:54:59 +0530

    Implemented services for Post Reaction Entity

---
+++ SocialMedia/Services/Interfaces/IPostReactionService.cs
@@ -0,0 +1,9 @@
+﻿using SocialMedia.models.DTO.PostReaction;
+
+namespace SocialMedia.Services.Interfaces
+{
+    public interface IPostReactionService
+    {
+        Task ToggleReactionAsync(ReactToPostDTO dto);
+    }
+}
---
+++ SocialMedia/models/DTO/PostReaction/ReactToPostDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SocialMedia.models.DTO.PostReaction
+{
+    public class ReactToPostDTO
+    {
+        public Guid PostId { get; set; }
+        public Guid UserId { get; set; }
+        public ReactionType Type { get; set; }
+    }
+}
