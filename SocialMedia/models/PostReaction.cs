2c5a1c5c1bd9a16cf5114db41b97f12719fe5c32
FarazUniGoa <mca2024.faraz@unigoa.ac.in>
2025-12-26 17:30:31 +0530

    Created the Post Reaction Enitity

---
+++ SocialMedia/models/PostReaction.cs
@@ -0,0 +1,23 @@
+﻿namespace SocialMedia.models
+{
+    public class PostReaction
+    {
+        public Guid Id { get; set; }
+
+        public Guid PostId { get; set; }
+        public Guid UserId { get; set; }
+
+        public ReactionType Type { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        
+    }
+
+
+    public enum ReactionType
+    {
+        Upvote=0,
+        Downvote=1
+    }
+
+}
