e388e9f3aee0cfe098c71a2f2a856b057883db79
FarazUniGoa <mca2024.faraz@unigoa.ac.in>
2025-12-26 18:00:09 +0530

    Implemented Post Reaction Controllers

---
+++ SocialMedia/Controllers/PostReactionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.models.DTO.PostReaction;
+using SocialMedia.Services.Interfaces;
+
+namespace SocialMedia.Controllers
+{
+    [ApiController]
+    [Route("reaction/{postId}")]
+    public class PostReactionController : ControllerBase
+    {
+        private readonly IPostReactionService _service;
+
+        public PostReactionController(IPostReactionService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> React(
+            Guid postId,
+            [FromBody] ReactToPostDTO dto)
+        {
+            if (postId != dto.PostId)
+                return BadRequest("PostId mismatch");
+
+            await _service.ToggleReactionAsync(dto);
+            return Ok();
+        }
+    }
+
+}
